9edd305a93afc9b3f2f55dcc3507195ee77f5334
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-10-03 16:06:08 -0400

    adding ui class

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', getWeather)
 
 function getWeather(){
   weather.getWeather()
-  .then(results => console.log(results))
+  .then(results => 
+  ui.paint(results))
   .catch(err => console.log(err))
 }
---
+++ ui.js
@@ -0,0 +1,26 @@
+class UI {
+  constructor(){
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.string = document.getElementById('w-string');
+    this.details = document.getElementById('w-details');
+    this.icon = document.getElementById('w-icon');
+    this.humidity = document.getElementById('w-humidity');
+    this.feelsLike = document.getElementById('w-feels-like');
+    this.uvIndex = document.getElementById('w-dew-point');
+    this.wind = document.getElementById('w-wind');
+  }
+
+  paint(weather){
+    this.location.textContent = weather.location.name
+    this.desc.textContent = weather.current.weather_descriptions
+    this.string.textContent = weather.current.temperature
+    this.details.textContent = weather.location.name
+    this.icon.setAttribute('src', weather.current.weather_icons)
+    this.humidity.textContent = `Humidity: ${weather.current.humidity}`
+    this.feelsLike.textContent = `Feels Like: ${weather.current.feelslike}`
+    this.uvIndex.textContent = `UV Index: ${weather.current.uv_index}`
+    this.wind.textContent = `Wind Speed: ${weather.location.name} mph`
+  }
+
+}
