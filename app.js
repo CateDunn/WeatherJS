abc8267d3396e5d26ddd665f958a99ca7c18909b
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-10-03 15:44:45 -0400

    added event listener for dom load

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //init weather object
 const weather = new Weather('Raleigh');
 
-weather.getWeather()
+//get weather on dom load
+document.addEventListener('DOMContentLoaded', getWeather)
+
+// weather.changeLocation('Charlotte')
+
+function getWeather(){
+  weather.getWeather()
   .then(results => console.log(results))
   .catch(err => console.log(err))
+}
